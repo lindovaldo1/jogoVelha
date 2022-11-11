94b25e8ade3e42f08ee96720c250a5aa0c8cab6b
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-11 01:51:22 -0300

    FIRST COMMIT

---
+++ app/src/main/java/com/inovatec/jogovelha/GamePlayActivity.kt
@@ -0,0 +1,11 @@
+package com.inovatec.jogovelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_play)
+    }
+}
